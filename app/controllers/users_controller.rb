24145ae4d76b563ec6f085df38b1f24acfde0dbf
Seth Strouf <seth.strouf@doane.edu>
2022-08-16 20:28:09 -0500

    Add Serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,31 +3,31 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    users = User.all
 
-    render json: @users
+    render json: UserSerializer.new(users).serializable_hash
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: UserSerializer.new(@user).serializable_hash
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      render json: UserSerializer.new(user).serializable_hash
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: UserSerializer.new(@user).serializable_hash
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).require(:attributes).permit(:name, :email)
     end
 end
